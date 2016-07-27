badcea515e29c2304eb3f1039162fea2e41fc284
KeenanJT <keenturner@gmail.com>
2016-07-27 08:46:00 -0400

    Changed bicolor and added constant

---
+++ getDemoTH/GitforiOS Notes.playground/Contents.swift
@@ -0,0 +1,32 @@
+// Git for iOS || Version Control
+
+/* Termonology
+ 
+ Repositories "Repos" - Git Projects - Contain all of the project files as well as their revision history. Conceeptually, they are like a folder that holds the whole project over time.
+ 
+ Commit - One or more changes to one or more files, accompanied by a brief commit message from the user. A unique id/hash/shaw will be made so you know which changes were commited. Something that should be preserved for the future.
+ 
+ Push - Sending local committed changes to the remote "repo". It will not merge to other active branches.
+ 
+ Pull - Getting committed changes from the remote "repo" while simultaneously merging them into local copy.
+ 
+ Branch - Parallel version of a "repo" that is housed within the repo, but doesn't affect other branches when changed. New branches for new features.
+ 
+ Main Branch - Is known as the master branch
+ 
+ Tip - Latest commit
+ 
+ Merge - Taking changes from one branch and inserting them into another. A merge can happena utomatically, like during a pull, or by explicitly merging one branch into another.
+ 
+ Diff - Difference between two commits
+ 
+ Gitignore - File that tells git to intentionally exclude certain files or folders
+ 
+ Git - Open source program for tracking changes
+ 
+ Github - Social and user interface, which uses Git as its primary machinery
+ 
+ 
+ 
+ */
+
